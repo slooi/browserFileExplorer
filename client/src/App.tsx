2c6b05e2bf928e207efc6b23580f17dc96b57fc1
s <s.looi2@outlook.com>
2025-03-16 01:47:34 +0900

    :sparkles::art: make files open in new tab and organize code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,25 +6,44 @@ function App() {
   const [data, setData] = useState<null | DirItem[]>(null)
 
 
-  async function fetchItems(path: string = "") {
-    const res = await fetch("/api?path=" + path)
+  async function fetchDirItems(path: string = "") {
+    const res = await fetch("/api?path=" + encodeURIComponent(path))
     const json = await res.json()
     console.log(json)
     setData(json)
   }
 
+  async function fetchFile(download: string) {
+    window.open("api?path=" + encodeURIComponent(download), "_blank")
+  }
+
+  function handleClick(item: DirItem) {
+    if (item.type === "dir") {
+      fetchDirItems(item.fullPath)
+    } else if (item.type === "file") {
+      fetchFile(item.fullPath)
+    } else {
+      throw new Error("UNEXPECTED ITEM TYPE")
+    }
+  }
+
   useEffect(() => {
-    fetchItems()
+    fetchDirItems()
   }, [])
 
   return (
     <>
       <h1>asd</h1>
       <ul>
-        {data && data.map(item => {
-          return <li key={item.itemName} className={"clickable"} onClick={() =>
-            fetchItems(item.fullPath)}><a href="#">{item.itemName}</a></li>
-        })}
+        {data && data.map(item => (
+          <li
+            key={item.itemName}
+            className={"clickable"}
+            onClick={() => handleClick(item)}
+          >
+            <a href="#">{item.itemName}</a>
+          </li>
+        ))}
       </ul>
     </>
   )
